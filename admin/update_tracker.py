c26d8fffe4d9fb3937e3ede0edfbc0e850e1550a
Donald Bowen <50885867+donbowen@users.noreply.github.com>
2022-10-04 13:12:18 -0400

    tracker now also makes weekly download fig

--- admin/update_tracker.py
+++ admin/update_tracker.py
@@ -39,14 +39,53 @@ df2.loc[:,'interpolated']  = df2.iloc[:2,1]
 df2.loc[:,'weekly'] = df2.iloc[1:,1]
 df2.Date = pd.to_datetime(df2.Date) 
 plot = df2.plot.line(x='Date',y=['interpolated','weekly'],
-                     style=['b.','b-'],legend=False,title='RETech Downloads')
+                     style=['b:','b-'],legend=False,title='RETech Downloads')
 
 plot.get_figure().savefig('temp.png', format="png")
 
 with open('temp.png','rb') as f:
-    file_to_update = repo.get_contents('admin/historical_DLs.png')
-    repo.update_file('admin/historical_DLs.png',
-                     'updating DL tracker fig',
+    file_to_update = repo.get_contents('admin/DLs_cumulative.png')
+    repo.update_file('admin/DLs_cumulative.png',
+                     'updating cumulative tracker fig',
+                     f.read(),
+                     file_to_update.sha
+                     )
+
+# let's plot weekly downloads (again with the slight interpolation complication)
+
+plot = (
+    df2.set_index('Date').drop('Count',axis=1)
+
+    # interpolate to "daily total download" tracker
+    .resample('D').interpolate(method='linear', limit_direction='forward', axis=0)
+
+    # interp column should be nan after it's over:
+    .assign(interpolated = lambda x: x.interpolated.where(x.interpolated != x.interpolated.shift(), None))
+
+    # convert to "daily download" tracker
+    .diff()
+
+    # convert to "weekly download" tracker
+    .resample('W').agg(pd.Series.sum, min_count=1)
+
+    # there is a week with some interpolated downloads and some known
+    # just allocate that weeks downloads to the interpolated
+    .assign(weekly       = lambda x: x.weekly.where(x.weekly.isnull() | x.interpolated.isnull(), x.weekly + x.interpolated))
+    .assign(interpolated = lambda x: x.interpolated.where(x.weekly.isnull() | x.interpolated.isnull(), None))
+
+    # ok, time to plot
+    [:-1] # skip latest week (partial, will always be low)
+    .reset_index()
+    .plot.line(x='Date',y=['interpolated','weekly'],style=['b:','b-'],
+               legend=False,title='Weekly Downloads of RETech Data')
+)
+
+plot.get_figure().savefig('temp2.png', format="png")
+
+with open('temp2.png','rb') as f:
+    file_to_update = repo.get_contents('admin/DLs_weekly.png')
+    repo.update_file('admin/DLs_weekly.png',
+                     'updating weekly tracker fig',
                      f.read(),
                      file_to_update.sha
                      )
